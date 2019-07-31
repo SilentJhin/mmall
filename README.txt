b057ac62536159bd1911ce1ec44bd2f2e0dd4e6f
Jhin <1171127959@qq.com>
2019-07-31 16:15:09 +0800

    add user modules

--- README.txt
+++ README.txt
@@ -1,9 +1,25 @@
 慕课商城后端项目代码
-V1.0    空白项目初始化
+V1  项目初始化
 maven pom文件初始化
 项目结构包初始化
 mybatis-generator配置    生成数据对象&时间戳优化
 mybatis-plugin配置    dao层和xml文件切换
 mybatis-pagehelper配置    分页插件
+
+V2  用户模块：
+    登录 验证 注册 忘记密码 重置密码
+    横纵向越权 高复用响应对象封装  MD5 Guava session
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -0,0 +1,99 @@
+package com.mmall.common;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+
+/**
+ * Created by SilentJhin.
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//保证xuliehjson时若为null对象则key也会消失
+public class ServerResponse<T> implements Serializable {
+
+    private int status;
+    private String msg;
+    private T data;
+
+    private ServerResponse(int status){
+        this.status = status;
+    }
+
+    private ServerResponse(int status,T data){
+        this.status = status;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg, T data){
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg){
+        this.status = status;
+        this.msg = msg;
+    }
+
+    @JsonIgnore
+    //使得不在json序列化结果中
+    public boolean isSuccess(){
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    }
+
+    //当需要msg作为data时调用此方法 通过方法重载规避构造器中无法使得string类型作为data保存
+    public static <T> ServerResponse<T> createBySuccess(String msg,T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    }
+
+    public static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    }
+
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMessage);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode,String errorMessage){
+        return new ServerResponse<T>(errorCode,errorMessage);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
