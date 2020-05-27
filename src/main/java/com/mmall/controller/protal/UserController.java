cc8913f443dee962504ce5b61693bc88e7f83565
caiguoqiang <guoqiang.cai@newtouch.com>
2020-05-27 17:39:16 +0800

    add CookieUtil

--- src/main/java/com/mmall/controller/protal/UserController.java
+++ src/main/java/com/mmall/controller/protal/UserController.java
@@ -9,14 +9,20 @@ import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+@Slf4j
 @Controller
 @RequestMapping("/user/")
 public class UserController {
@@ -33,11 +39,18 @@ public class UserController {
      */
     @RequestMapping(value = "login.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login (String username, String password, HttpSession session){
+    public ServerResponse<User> login (String username, String password, HttpSession session,
+                                       HttpServletResponse httpServletResponse, HttpServletRequest httpServletRequest){
         //service-->mybatis-->dao
         ServerResponse<User> response = iUserService.login(username,password);
         if (response.isSuccess()){
-            session.setAttribute(Const.CURRENT_USER,response.getData());
+            CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+            String cookieValue = CookieUtil.readLoginToken(httpServletRequest);
+            CookieUtil.delLoginToken(httpServletRequest, httpServletResponse);
+            log.info(cookieValue);
+            //session.setAttribute(Const.CURRENT_USER,response.getData());
+            // 本来保存在session里的用户信息 放到数据库里 有效期 30min
+            RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()), Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
 
         return response;
---
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -0,0 +1,59 @@
+package com.mmall.util;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CookieUtil {
+
+    private final static String COOKIE_DOMAIN = ".happymmall.com";
+    private final static String COOKIE_NAME = "mmall_login_token";
+
+    // 把request对象里的所有cookie保存到cookie数组里,遍历数组拿到指定的cookie后返回其值
+    public static String readLoginToken (HttpServletRequest request) {
+        Cookie[] cks = request.getCookies();
+        if (cks != null) {
+            for (Cookie ck : cks) {
+                log.info("read cookieName:{}, cookieValue:{}", ck.getName(), ck.getValue());
+                if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
+                    log.info("return cookieName:{}, cookieValue:{}", ck.getName(), ck.getValue());
+                    return ck.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    // 根据传入的tooken新建cookie对象，将其保存到response对象里
+    public static void writeLoginToken (HttpServletResponse response, String token) {
+        Cookie ck = new Cookie(COOKIE_NAME, token);
+        ck.setDomain(COOKIE_DOMAIN);
+        ck.setPath("/"); // 代表设置在根目录
+        // 如果maxage不设置，cookie就不会写入硬盘而是写在内存里，只在当前页面有效
+        ck.setMaxAge(60 * 60 * 24 * 265);// -1 永久 单位是秒
+        log.info("write cookieName:{}, cookieValue: {}", ck.getName(), ck.getValue());
+        response.addCookie(ck);
+    }
+
+    // 从request里拿到cookie数组，找到指定的cookie，设置删除后添加到response里
+    public static void delLoginToken (HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cks = request.getCookies();
+        if (cks != null) {
+            for (Cookie ck : cks) {
+                if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
+                    ck.setDomain(COOKIE_DOMAIN);
+                    ck.setPath("/");
+                    ck.setMaxAge(0);// 0为删除cookie
+                    log.info("del cookieName:{}, cookieValue:{}", ck.getName(), ck.getValue());
+                    response.addCookie(ck);
+                    return;
+                }
+            }
+        }
+    }
+}
